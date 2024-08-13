95472f46faa75a1df9d7ac6492b75c963fae5377
Sujan Maharjan <sujanmh@noveltytechnology.com>
2024-08-13 21:32:00 +0545

    Menu & Dialog practice

--- app/src/main/java/com/example/unit5demo/MainActivity.java
+++ app/src/main/java/com/example/unit5demo/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.unit5demo;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,6 +19,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.unit5demo.fragments.BlueFragment;
 import com.example.unit5demo.fragments.PinkFragment;
+import com.example.unit5demo.meme.MemeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,5 +51,36 @@ public class MainActivity extends AppCompatActivity {
         transaction.replace(R.id.fragmentView, fragment);
 
         transaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_option, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_home) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragmentView, new MemeFragment())
+                    .commit();
+
+        } else if (item.getItemId() == R.id.menu_alert_dialog) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder
+                    .setTitle("🔔 Alert Triggered!")
+                    .setMessage("Are you enjoying?")
+                    .setPositiveButton("Yes", (dialog, id) -> dialog.dismiss())
+                    .setNegativeButton("No", (dialog, id) -> dialog.dismiss())
+                    .create()
+                    .show();
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/unit5demo/meme/MemeFragment.java
+++ app/src/main/java/com/example/unit5demo/meme/MemeFragment.java
@@ -1,12 +1,20 @@
 package com.example.unit5demo.meme;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.unit5demo.R;
@@ -28,9 +36,74 @@ public class MemeFragment extends Fragment {
         memeImgView3 = view.findViewById(R.id.memeImageView3);
 
         // Registering context menu
+        registerForContextMenu(memeImgView1);
 
         // Setting click listeners for popup menu
+        memeImgView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(requireContext(), view);
+
+                MenuInflater popMenuInflater = popupMenu.getMenuInflater();
+                popMenuInflater.inflate(R.menu.menu_popup, popupMenu.getMenu());
+
+//                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+//                    @Override
+//                    public boolean onMenuItemClick(MenuItem item) {
+//                        return false;
+//                    }
+//                });
+
+                popupMenu.setOnMenuItemClickListener(item -> {
+                    if (item.getItemId() == R.id.menu_like) {
+                        Toast
+                                .makeText(
+                                        getContext(),
+                                        "Liked!",
+                                        Toast.LENGTH_SHORT
+                                )
+                                .show();
+                        return true;
+                    }
+                    return false;
+                });
+
+                popupMenu.show();
+
+            }
+        });
 
         return view;
     }
+
+    @Override
+    public void onCreateContextMenu(@NonNull ContextMenu menu, @NonNull View v, @Nullable ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater menuInflater = requireActivity().getMenuInflater();
+        menuInflater.inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_share) {
+
+        } else if (item.getItemId() == R.id.menu_custom_dialog) {
+            Dialog customDialog = new Dialog(requireContext());
+            customDialog.setContentView(R.layout.dialog_custom);
+
+            Button customDialogCloseButton = customDialog
+                    .findViewById(R.id.dialog_button);
+            customDialogCloseButton.setOnClickListener(view ->
+                    customDialog.dismiss()
+            );
+
+            customDialog.show();
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
+//    private void showPopMenu(View view){
+//
+//    }
 }
